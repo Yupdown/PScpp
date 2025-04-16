845f1a561df947a845d7ba82ceb052c561d821ec
Yup <do.yupdown@gmail.com>
2025-04-16 21:37:17 +0900

    Apr 16

--- bojc++/BOJ13925.cpp
+++ bojc++/BOJ13925.cpp
@@ -14,14 +14,9 @@ pair<int64, int64> merge_segment_task(const pair<int64, int64>& lhs, const pair<
 	return make_pair((lhs.first * rhs.first) % MOD, (rhs.first * lhs.second + rhs.second) % MOD);
 }
 
-bool is_segment_applied(int i)
+void apply_segment_task(int i, int lv)
 {
-	return task[i] == make_pair(1, 0);
-}
-
-void apply_segment_task(int i, int64 n)
-{
-	table[i] = (table[i] * task[i].first + task[i].second * n) % MOD;
+	table[i] = (table[i] * task[i].first + task[i].second * (1ll << lv)) % MOD;
 	if (i < SEGBASE)
 	{
 		task[i << 1] = merge_segment_task(task[i << 1], task[i]);
@@ -30,62 +25,51 @@ void apply_segment_task(int i, int64 n)
 	task[i] = make_pair(1, 0);
 }
 
-void update_segment_table(int l, int r)
+void add_segment(int l, int r, const pair<int64, int64>& src, int lv)
 {
-	if (!l && !r)
-		return;
-	if (l < SEGBASE)
-		table[l] = (table[l << 1] + table[l << 1 | 1]) % MOD;
-	if (r < SEGBASE)
-		table[r] = (table[r << 1] + table[r << 1 | 1]) % MOD;
-	update_segment_table(l >> 1, r >> 1);
-}
+	int lp = l >> lv;
+	int rp = r >> lv;
 
-void update_segment_task(int l, int r, int64 n)
-{
-	if (l || r)
-		update_segment_task(l >> 1, r >> 1, n << 1);
-	apply_segment_task(l, n);
-	apply_segment_task(l & ~1, n);
-	apply_segment_task(r, n);
-	apply_segment_task(r | 1, n);
-}
-
-void add_segment_task(int l, int r, const pair<int64, int64>& src, int64 n)
-{
-	if (l < r)
-		add_segment_task((l + 1) >> 1, (r - 1) >> 1, src, n << 1);
-	if (l & 1)
+	if (rp - lp > 1)
 	{
-		task[l] = merge_segment_task(task[l], src);
-		apply_segment_task(l, n);
-		apply_segment_task(l & ~1, n);
-		if (l < SEGBASE)
-		{
-			apply_segment_task(l << 1, n >> 1);
-			apply_segment_task(l << 1 | 1, n >> 1);
-		}
+		if (~lp & 1)
+			task[lp + 1] = merge_segment_task(task[lp + 1], src);
+		if (rp & 1)
+			task[rp - 1] = merge_segment_task(task[rp - 1], src);
 	}
-	if (~r & 1)
+
+	apply_segment_task(lp, lv);
+	apply_segment_task(lp ^ 1, lv);
+	apply_segment_task(rp, lv);
+	apply_segment_task(rp ^ 1, lv);
+
+	if (lv > 0)
 	{
-		task[r] = merge_segment_task(task[r], src);
-		apply_segment_task(r, n);
-		apply_segment_task(r | 1, n);
-		if (r < SEGBASE)
+		add_segment(l, r, src, lv - 1);
+		table[lp] = (table[lp << 1] + table[lp << 1 | 1]) % MOD;
+		table[rp] = (table[rp << 1] + table[rp << 1 | 1]) % MOD;
+	}
+	else
+	{
+		task[lp] = merge_segment_task(task[lp], src);
+		apply_segment_task(lp, lv);
+		if (lp < rp)
 		{
-			apply_segment_task(r << 1, n >> 1);
-			apply_segment_task(r << 1 | 1, n >> 1);
+			task[rp] = merge_segment_task(task[rp], src);
+			apply_segment_task(rp, lv);
 		}
 	}
 }
 
 int64 get_segment(int l, int r)
 {
-	int64 ret = l < r ? get_segment((l + 1) >> 1, (r - 1) >> 1) : 0;
+	int64 ret = 0;
 	if (l & 1)
-		ret += table[l];
+		ret += table[l++];
 	if (~r & 1)
-		ret += table[r];
+		ret += table[r--];
+	if (l < r)
+		ret += get_segment(l >> 1, r >> 1);
 	return ret % MOD;
 }
 
@@ -97,8 +81,8 @@ int main()
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 		cin >> table[SEGBASE + i];
-	for (int i = SEGBASE - 1; i >= 0; --i)
-		table[i] = table[i << 1] + table[i << 1 | 1];
+	for (int i = SEGBASE - 1; i > 0; --i)
+		table[i] = (table[i << 1] + table[i << 1 | 1]) % MOD;
 	fill(begin(task), end(task), make_pair(1, 0));
 	cin >> m;
 	for (int i = 0; i < m; ++i)
@@ -124,15 +108,11 @@ int main()
 				src = make_pair(0, v);
 				break;
 			}
-			update_segment_task(l, r, 1);
-			add_segment_task(l, r, src, 1);
-			update_segment_table(l, r);
+			add_segment(l, r, src, 17);
 		}
 		else
 		{
-
-			update_segment_task(l, r, 1);
-			update_segment_table(l, r);
+			add_segment(l, r, make_pair(1, 0), 17);
 			cout << get_segment(l, r) << "\n";
 		}
 	}
